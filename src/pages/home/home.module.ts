c5686816cceed607991f3e0db9610c3887d435c4
nateodes <nateodes@gmail.com>
2017-10-01 13:20:17 -0600

    playing with stylig, ion elements

--- src/pages/home/home.module.ts
+++ src/pages/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { HomePage } from './home';
 import { Facebook } from '@ionic-native/facebook';
+import { List } from 'ionic-angular';
 
 
 
